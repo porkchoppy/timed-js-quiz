06c6aa823092e74ff299ab74140c891c4019cbb1
Christin Carter <raspberry.muppet@gmail.com>
2022-11-06 12:02:23 -0700

    questions for quiz created

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+// 1. What is JavaScript?
+// a) A scripting language used to make a website interactive
+// b) A web hosting site
+// c) The name of the robot on Futurama
+// d) None of the above
+
+// 2. What do you call a JavaScript variable encased in quotation marks? 
+// a) A number 
+// b) A statement
+// c) A string
+// d) A quote
+
+// 3. Which of the following is true about let and const? 
+// a) Let and const are globally scoped. 
+// b) Let can be reassigned while const cannot. 
+// c) Both of the above. 
+// d) None of the above. 
+
+// 4. What is an array? 
+// a) A square bracketed list of values separated by commas
+// b) A single variable that is used to store different elements
+// c) Both of the above
+// d) None of the above 
+
+// 5. Which function is used to add an element to the end of an array and return the length of the array? 
+// a) join()
+// b) pop()
+// c) push()
+// d) add()
+
+// 6. Which of the following is false about null? 
+// a) It is an object that can be assigned to a variable as a representation of no value.
+// b) When used in arithmetic operations, null will be converted to zero behind the scenes.
+// c) When used in conditional logic, null will return as false. 
+// d) Null is not considered a primitive value in JavaScript.

4c5ec98fa8e6801dc9e0294785e6100aca842bca
Christin Carter <raspberry.muppet@gmail.com>
2022-11-07 23:42:33 -0700

    created readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# timed-js-quiz
+# Timed-JS-Quiz
+
+## Description
+This repository links to a timed quiz in which there are six multiple choice questions to test your knowledge on JavaScript. The quiz is timed, and each time an incorrect answer is selected 5 seconds are docked from the timer. The quiz is over when all questions have been answered or the timer has run out. At the end of the quiz, there is an option for you to log your score and initials on the scoreboard. 
+
+
+## Usage
+
+Here is an example of the questions and the timer counting down.
+
+![questions-1 screenshot](assets/images/x)
+
+![questions-2 screenshot](assets/images/x)
+
+Along with a screenshot of the scoreboard.
+
+![scoreboard screenshot](assets/images/x)
+
+## Credits
+
+I received collaborative brainstorming and help from my tutor, Andrew Tirpok. 
+
+## Link to live site
+
+https://porkchoppy.github.io/timed-js-quiz
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,53 @@
 var questions = [
     {
-    question: "What is JavaScript",
-    answerA: "A scripting language used to make a website interactive",
-    answerB: "A web hosting site",
-    answerC: "The name of the robot on Futurama",
-    answerD: "None of the above",
-    correctAnswer: ""
+    question: "1. What is JavaScript",
+    answerA: "a) A scripting language used to make a website interactive",
+    answerB: "b) A web hosting site",
+    answerC: "c) The name of the robot on Futurama",
+    answerD: "d) None of the above",
+    correctAnswer: "a) A scripting language to make a website interactive"
   },
   {
-    question: "What is JavaScript",
-    answerA: "A scripting language used to make a website interactive",
-    answerB: "A web hosting site",
-    answerC: "The name of the robot on Futurama",
-    answerD: "None of the above"
-  }
+    question: "2. What do you call a JavaScript variable encased in quotation marks?",
+    answerA: "a) A number",
+    answerB: "b) A statement",
+    answerC: "c) A string",
+    answerD: "d) A quote",
+    correctAnswer: "c) A string"
+  }, 
+  {
+    question: "3. Which of the following is true about let and const?",
+    answerA: "a) Let and const are globally scoped.",
+    answerB: "b) Let can be reassigned while const cannot.",
+    answerC: "c) Both of the above",
+    answerD: "d) None of the above", 
+    correctAnswer: "b) Let can be reassigned while const cannot."
+  }, 
+  {
+    question: "4. What is an array?",
+    answerA: "a) A square bracketed list of values separated by commas",
+    answerB: "b) A single variable that is used to store different elements",
+    answerC: "c) Both of the above",
+    answerD: "d) None of the above",
+    correctAnswer: "c) Both of the above"
+  },
+  {
+    question: "5. Which function is used to add an element to the end of an array and return the length of the array?", 
+    answerA: "a) join()",
+    answerB: "b) pop()",
+    answerC: "c) push()",
+    answerD: "d) add()",
+    correctAnswer: "c) push()"
+},
+{
+    question: "6. Which of the following is false about null?", 
+    answerA: "a) It is an object that can be assigned to a variable as a representation of no value.",
+    answerB: "b) When used in arithmetic operations, null will be converted to zero behind the scenes.",
+    answerC: "c) When used in conditional logic, null will return as false.",
+    answerD: "d) Null is not considered a primitive value in JavaScript.",
+    correctAnswer: "d) Nulls is not considered a primitive value in JavaScript."
+}
 ]
-
-
-
-// 1. What is JavaScript?
-// a) A scripting language used to make a website interactive
-// b) A web hosting site
-// c) The name of the robot on Futurama
-// d) None of the above
-
-// 2. What do you call a JavaScript variable encased in quotation marks? 
-// a) A number 
-// b) A statement
-// c) A string
-// d) A quote
-
-// 3. Which of the following is true about let and const? 
-// a) Let and const are globally scoped. 
-// b) Let can be reassigned while const cannot. 
-// c) Both of the above. 
-// d) None of the above. 
-
-// 4. What is an array? 
-// a) A square bracketed list of values separated by commas
-// b) A single variable that is used to store different elements
-// c) Both of the above
-// d) None of the above 
-
-// 5. Which function is used to add an element to the end of an array and return the length of the array? 
-// a) join()
-// b) pop()
-// c) push()
-// d) add()
-
-// 6. Which of the following is false about null? 
-// a) It is an object that can be assigned to a variable as a representation of no value.
-// b) When used in arithmetic operations, null will be converted to zero behind the scenes.
-// c) When used in conditional logic, null will return as false. 
-// d) Null is not considered a primitive value in JavaScript.
 
 var startBtn = document.getElementById("start-button")
 var infoBox = document.getElementById("info-box")

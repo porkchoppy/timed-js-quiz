5d91e21cf6cb023bd57e1d2a585cb51affb6dbe2
Christin Carter <raspberry.muppet@gmail.com>
2023-01-30 19:44:09 -0700

    restructured questions and variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,75 @@
 //structuring the questions to be included in the quiz
-var questions = [
+const questions = [
    {
     question: "What is JavaScript?",
-    answers: ["A scripting language used to make a website interactive", "A web hosting site", "The name of the robot on Futurama", "None of the above"],
-    correctAnswer: 0
+    a: "A scripting language used to make a website interactive", 
+    b: "A web hosting site", 
+    c: "The name of the robot on Futurama", 
+    d: "None of the above",
+    correctAnswer: "a",
    },
    {
     question: "What do you call a JavaScript variable encased in quotation marks?",
-    answers: ["A number", "A statement", "A string", "A quote"],
-    correctAnswer: 2
+    a: "A number", 
+    b: "A statement", 
+    c: "A string", 
+    d: "A quote",
+    correctAnswer: "c",
    }, 
    {
     question: "Which of the following is true about let and const?",
-    answers: ["Let and const are globally scoped.", "Let can be reassigned while const cannot.", "Both of the above", "None of the above"], 
-    correctAnswer: 1
+    a: "Let and const are globally scoped.", 
+    b: "Let can be reassigned while const cannot.", 
+    c: "Both of the above", 
+    d: "None of the above", 
+    correctAnswer: "b",
    }, 
    {
     question: "What is an array?",
-    answers: ["A square bracketed list of values separated by commas", "A single variable that is used to store different elements", "Both of the above", "None of the above"],
-    correctAnswer: 2
+    a: "A square bracketed list of values separated by commas", 
+    b: "A single variable that is used to store different elements", 
+    c: "Both of the above", 
+    d: "None of the above",
+    correctAnswer: "c",
    },
    {
     question: "Which function is used to add an element to the end of an array and return the length of the array?", 
-    answers: ["join()", "pop()", "push()", "add()"],
-    correctAnswer: 2
+    a: "join()", 
+    b: "pop()", 
+    c: "push()", 
+    d: "add()",
+    correctAnswer: "c",
    },
    {
     question: "Which of the following is false about null?", 
-    answers: ["It is an object that can be assigned to a variable as a representation of no value.", "When used in arithmetic operations, null will be converted to zero behind the scenes.", "When used in conditional logic, null will return as false.", "Null is not considered a primitive value in JavaScript."],
-    correctAnswer: 3
+    a: "It is an object that can be assigned to a variable as a representation of no value.", 
+    b: "When used in arithmetic operations, null will be converted to zero behind the scenes.", 
+    c: "When used in conditional logic, null will return as false.", 
+    d: "Null is not considered a primitive value in JavaScript.",
+    correctAnswer: "d",
    }
 ];
 
-var startBtn = document.getElementById("start-button")
-var infoBox = document.getElementById("info-box")
-var questionBox = document.getElementById("questionBox")
-var questionIndex = 0
+// variables to manuever the quiz questions via the homepage
+const quiz = document.getElementById("quiz");
+const answer = document.getElementById(".answer");
+const question = document.getElementById("question");
+const textA = document.getElementById("textA");
+const textB = document.getElementById("textB");
+const textC = document.getElementById("textC");
+const textD = document.getElementById("textD");
+const submitButton = document.getElementById("submit");
+const timer = document.getElementById("timer");
+const score = document.getElementById("score");
+
+let currentQuiz = 0;
+let currentScore = 0;
+let timeLeft = 60;
+
+// var startBtn = document.getElementById("start-button")
+// var infoBox = document.getElementById("info-box")
+// var questionBox = document.getElementById("questionBox")
+// var questionIndex = 0
 
 startBtn.addEventListener("click", function(){
     infoBox.style.display = "none"
